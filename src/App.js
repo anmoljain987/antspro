0c09702e26cec2653d0f468e6f2b00c3e40a5579
Anmol Jain <anmoljain987@gmail.com>
2022-06-17 18:12:10 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import "./App.css";
 import { useState } from "react";
 
 import NavigationBar from "./components/Home/NavigationBar";
-
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import { auth } from "./utils/fire";
 import RoutesMain from "./Routes/RoutesMain";
 
 function App() {
-  // const [user] = useAuthState(auth);
   const [dataSource, setDataSource] = useState([]);
 
   return (
